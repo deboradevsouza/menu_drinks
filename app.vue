18006d624efd920627dcadafe6f8f3df059d30d2
deboradevsouza <souza.deboravieira@gmail.com>
2025-07-28 15:37:14 -0300

    Adding header and first drink

---
+++ app.vue
@@ -0,0 +1,54 @@
+
+<template>
+  <body class="flex flex-col items-center bg-[#BCCBAA]">
+    
+    <div class="flex flex-col bg-[#fff] rounded-md p-4 m-4 w-full h-full">
+      <header class="flex flex-col items-center m-0">
+        <h1 class="flex">Manoela & Fabio</h1>
+        <h2 class="flex self-end">menu drinks</h2>
+      </header>
+
+      <br><hr><br>
+
+      <div class="flex flex-row items-center self-center p-4 w-[300px] h-[150px]">
+        <img src="/assets/Drink_Jorginho.png" height="150" width="150" alt="drink">
+        <div class="flex flex-col justify-between">
+          <h3 class="flex items-center">Jorginho</h3>
+          <p class="flex self-center text-center">Gim, suco de limão siciliano, xarope de framboesa e clara de ovo</p>
+        </div>
+      </div>
+      </div>
+  </body>
+</template>
+
+<style scoped>
+h1 {
+  font-family: "Lexend Exa", sans-serif;
+  font-size: 25px;
+  font-weight: 100;
+  text-transform: uppercase;
+  color: #40370D;
+}
+
+h2 {
+  font-family: "WindSong", sans-serif;
+  font-size: 30px;
+  font-weight: 500;
+  max-width: fit-content;
+  color: #40370D;
+}
+
+h3 {
+  font-family: "WindSong", sans-serif;
+  font-size: 25px;
+  font-weight: 500;
+  max-width: fit-content;
+  color: #40370D;
+}
+
+p {
+  font-family: "Roboto", sans-serif;
+  font-size: 12px;
+  color: #40370D;
+}
+</style>
